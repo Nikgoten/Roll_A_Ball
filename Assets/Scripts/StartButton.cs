5d3cf67833a5b21ae05251cbb8c88e548e3bb04b
Niklas Weber <niklas_weber.1403@gmx.de>
2015-12-21 21:12:00 +0100

    Programmed title screen and tested the basic functions of the game via test level. fixed lighting problems

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour {
+
+    public Button startButton;
+
+    //public Button exitButton;
+   
+    
+    void Start () {
+
+        startButton = startButton.GetComponent<Button> ();
+
+        //exitButton = exitButton.GetComponent<startButton>();
+        
+	}
+
+    public void StartLevel ()
+    {
+        Application.LoadLevel (1);
+    }
+	
+	// Update is called once per frame
+	
+}
